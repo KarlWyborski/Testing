034ba16e61c506acdba04bbd68d676bcc08a2aff
Karl Wyborski <KarlWyborski66@gmail.com>
2019-02-15 10:46:55 -0500

    HTTP Server

--- WebServer.py
+++ WebServer.py
@@ -1,46 +1,41 @@
-#!/usr/bin/env python
-"""
-Very simple HTTP server in python.
-Usage::
-    ./dummy-web-server.py [<port>]
-Send a GET request::
-    curl http://localhost
-Send a HEAD request::
-    curl -I http://localhost
-Send a POST request::
-    curl -d "foo=bar&bin=baz" http://localhost
-"""
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
+import http.server
+import socketserver
 
-class S(BaseHTTPRequestHandler):
+##PORT = 8080
+Handler = http.server.BaseHTTPRequestHandler
+
+class S(Handler):
     def _set_headers(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-Type', 'text/html')
         self.end_headers()
-
+    
     def do_GET(self):
         self._set_headers()
-        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
-
+        self.wfile.write(b"<html><body><h1>Hi!</h1></body></html>")
+    
     def do_HEAD(self):
         self._set_headers()
-        
+    
     def do_POST(self):
-        # Doesn't do anything with posted data
         self._set_headers()
-        self.wfile.write("<html><body><h1>POST!</h1></body></html>")
-        
-def run(server_class=HTTPServer, handler_class=S, port=80):
-    server_address = ('', port)
+        self.wfile.write(b"<html><body><h1>POST!</h1></body></html>")
+
+def run(server_class=http.server.HTTPServer, handler_class=S, PORT = 8080):
+    server_address = ('',PORT)
     httpd = server_class(server_address, handler_class)
     print('Starting httpd...')
     httpd.serve_forever()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     from sys import argv
-
+    
     if len(argv) == 2:
         run(port=int(argv[1]))
     else:
         run()
+
+
+##    
+##print("serving at port", PORT)
+##socketserver.TCPServer(("",PORT), Handler).serve_forever()
