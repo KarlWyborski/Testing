03a8320793cf3cdba5e3434291c2c9a87dd9479e
Karl Wyborski <KarlWyborski66@gmail.com>
2019-02-27 13:47:47 -0500

    more socket

---
+++ SocketClient.py
@@ -0,0 +1,257 @@
+import socket
+import threading
+import sys
+from datetime import datetime
+import time
+import tkinter
+from tkinter import *
+import os
+
+
+def rcvMsg():
+    global iAddTime
+    global iCurrentTime
+    global bSET
+    global hourOffset
+    global minOffset
+    global secOffset
+    
+    data = 'a'
+    while True:
+        data = sock.recv(1024)
+        if not data:
+            break
+        data = data.decode('utf-8').split('\n')
+        print(data)
+        max = 6
+        if len(data) < max:
+            max = len(data)
+        else:
+            pass
+        i = 0
+        # i = 0
+        tempData = data[i].split(',')
+        bSET = tempData[0]
+        iAddTime = tempData[1]
+        hourOffset = int(tempData[2])
+        minOffset = int(tempData[3])
+        secOffset = int(tempData[4])
+        
+        if bSET == 'True':
+            lAddTime.configure(text=str(iAddTime))
+        else:
+            lAddTime.configure(text='')
+        
+        i += 1
+        
+        while i < max:
+            tempData = data[i].split(',')
+            print(i)
+            print(tempData)
+            if int(tempData[4]) > 12:
+                lStamps[i-1].configure(text=str(int(tempData[4])-12) + ':' + tempData[5] +' PM')
+            elif int(tempData[4]) == 0:
+                lStamps[i-1].configure(text='12:' + tempData[5] + ' AM')
+            elif int(tempData[4]) < 12:
+                lStamps[i-1].configure(text=tempData[4]+ ':' + tempData[5] +' AM')
+            elif int(tempData[4]) == 12:
+                lStamps[i-1].configure(text='12:' + tempData[5] + ' PM')
+                
+            lTimes[i-1].configure(text=tempData[0] + ' - ' + str(int(tempData[0]) + 5))
+            i += 1
+            
+        
+        
+            
+            
+            
+        
+
+def connect():
+    global connState
+    while not connState:
+        try:
+            print('Connecting...')
+            sock.connect((ipAddress, PORT))
+            print('Connected')
+            startRcvThread()
+            connState = True
+        except ConnectionRefusedError:
+            sock.close()
+            print('Connection Failed. Retrying in 10...')
+            time.sleep(10)
+
+def startRcvThread():
+    rcvThread = threading.Thread(target=rcvMsg)
+    rcvThread.daemon = True
+    rcvThread.start()
+    print('Rcv Thread Started.')
+
+##def startSendThread():
+##    time.sleep(1)
+##    sendThread = threading.Thread(target=sendMsg)
+##    sendThread.daemon = True
+##    sendThread.start()
+##    print('Send Thread Started.')
+    
+def cleanup():
+    sock.close()
+    win.destroy()
+
+def onKeyPress(event):
+    key = event.keysym
+##    print("You Pressed %s" % (key))
+    if key == 'Prior':
+        sendServer('DOWN')
+    elif key == 'Next':
+        sendServer('UP')
+    elif key == 'H' or key == 'Escape':
+        sendServer('SET')
+    elif key == 'b':
+        sendServer('OTHER')
+    
+def sendServer(msg):
+    print('Sending: ' + msg)
+    sock.send((msg).encode('utf-8'))
+
+def startClockThread():
+    clockThread = threading.Thread(target=clockChange)
+    clockThread.daemon = True
+    clockThread.start()
+    print('clock Thread Started.')
+
+def clockChange():
+    while True:
+        temp = adjustTime()
+        
+        if temp[3] > 12:
+            hour = str(temp[3] - 12)
+        elif temp[3] == 0:
+            hour = '12'
+        elif temp[3] < 10:
+            hour = '0'+ str(temp[3])
+        else:
+            hour = str(temp[3])
+        
+        if temp[3] > 12:
+            AMPM = ' PM'
+        else:
+            AMPM = ' AM'
+        
+        if temp[4] > 9:
+            min = str(temp[4])
+        else:
+            min = '0' + str(temp[4])
+        
+        if temp[5] > 9:
+            sec = str(temp[5])
+        else:
+            sec = '0' + str(temp[5])
+        
+        clock.configure(text=hour+':'+min+':'+sec+AMPM)
+        time.sleep(0.2)
+    
+
+def adjustTime():
+    tempHour = 0
+    tempMin = 0
+    
+    inTime = [0,0,0,0,0,0,0,0,0]
+    i = 0
+    while i < 9:
+        inTime[i] = time.localtime()[i]
+        i += 1
+    
+    if (inTime[5] + secOffset) > 59:
+        tempMin = 1
+        inTime[5] = inTime[5] + secOffset - 60
+    elif (inTime[5] + secOffset) < 0:
+        tempMin = -1
+        inTime[5] = inTime[5] + secOffset + 60
+    else:
+        inTime[5] = inTime[5] + secOffset
+    
+    if (inTime[4] + minOffset + tempMin) > 59:
+        tempHour = 1
+        inTime[4] = inTime[4] + minOffset + tempMin - 60
+    elif (inTime[4] + minOffset + tempMin) < 0:
+        tempHour = -1
+        inTime[4] = inTime[4] + minOffset + tempMin + 60
+    else:
+        inTime[4] = inTime[4] + minOffset + tempMin
+    
+    inTime[3] = inTime[3] + hourOffset + tempHour
+    return inTime
+    
+#--------------------------------------------------------
+#Sets up TKinter window
+win = Tk()
+
+clock = Label(win, text="insert time", font = 'Times 70', bg='gray70')
+clock.pack(side = BOTTOM, fill=X)
+
+textCurrentTime = Label(win, bg='White', height=1, text='Current CO Time', font='Times 30')
+textCurrentTime.pack(fill=X)
+
+lCurrentTime = Label(win, bg='White', text="## - ##", font = 'Times 100')
+lCurrentTime.pack(fill=X)
+
+lAddTime = Label(win, bg='White', text="", font = 'Times 70')
+lAddTime.pack(fill=X)
+
+lCurrentStamp = Label(win, bg='pale green', height = 2, text="Updated at: HH:MM", font = 'Times 40')
+lCurrentStamp.pack(fill=X)
+
+lHistTime1 = Label(win, pady=15, bg='gray70', text="-- - --", font = 'Times 70')
+lHistTime1.pack(fill=X)
+
+lHistStamp1 = Label(win, pady=15, anchor=SE, bg='gray70', text="HH:MM", font = 'Times 40')
+lHistStamp1.pack(fill=X)
+
+lHistTime2 = Label(win, pady=15, bg='White', text="-- - --", font = 'Times 70')
+lHistTime2.pack(fill=X)
+
+lHistStamp2 = Label(win, pady=15, anchor=SE, bg='White', text="HH:MM", font = 'Times 40')
+lHistStamp2.pack(fill=X)
+
+lHistTime3 = Label(win, pady=15, bg='gray70', text="-- - --", font = 'Times 70')
+lHistTime3.pack(fill=X)
+
+lHistStamp3 = Label(win, pady=15, anchor=SE, bg='gray70', text="HH:MM", font = 'Times 40')
+lHistStamp3.pack(fill=X)
+
+lHistTime4 = Label(win, pady=15, bg='White', text="-- - --", font = 'Times 70')
+lHistTime4.pack(fill=X)
+
+lHistStamp4 = Label(win, pady=15, anchor=SE, bg='White', text="HH:MM", font = 'Times 40')
+lHistStamp4.pack(fill=X)
+
+lTimes = [lCurrentTime,lHistTime1,lHistTime2,lHistTime3,lHistTime4]
+lStamps = [lCurrentStamp,lHistStamp1,lHistStamp2,lHistStamp3,lHistStamp4]
+    
+#-------------------------------------------------------
+#Sets up socket connection
+    
+
+ipAddress = sys.argv[1]
+PORT = 12343
+
+sRequest = 'PlaceHolder'
+connState = False
+iAddTime = 0
+iCurrentTime = 0
+bSET = False
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+hourOffset=0
+minOffset=0
+secOffset=0
+
+
+connect()
+startClockThread()
+##startSendThread()
+
+win.protocol("WM_DELETE_WINDOW", cleanup)
+win.bind('<Key>', onKeyPress)
+win.mainloop()
--- SocketServer.py
+++ SocketServer.py
@@ -8,49 +8,198 @@ from tkinter import *
 import os
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connections = []
-iCurrentTime = 0
-iCurrentStamp = 0
-iAddTime = 0
-bToggle = False
-sHistFileName = "/home/pi/DeweysProject/Testing/test.txt"
 
-
-self.sock.bind(('0.0.0.0', 12345))
-self.sock.listen(1)
-self.startAccThread()
 
 ##----------
-def startAccThread(self):
-    accThread = threading.Thread(target=self.acceptor)
+def startAccThread():
+    accThread = threading.Thread(target=acceptor)
     accThread.daemon = True
     accThread.start()
 
-def acceptor(self):
+def acceptor():
+    global connections
+    
     while True:
-        c, a = self.sock.accept()
-        cThread = threading.Thread(target=self.handler, args=(c,a))
+        c, a = sock.accept()
+        cThread = threading.Thread(target=handler, args=(c,a))
         cThread.daemon = True
         cThread.start()
-        self.connections.append(c)
+        connections.append(c)
         print(str(a[0]) + ':' + str(a[1]), "connected")
 
-def handler(self, c, a):
+def handler(c, a):
+    time.sleep(2)
+    c.send((str_allData()).encode('utf-8'))
     while True:
         data = c.recv(1024).decode('utf-8')
         print(data)
-        if data == 'CurrentTime':
-            
-            c.send(str(self.iCurrentTime).encode('utf-8'))
-        elif data == 'FullHist':
-            c.send((self.sHistTimes[0] + ',' + self.sHistTimes[1] + ',' + self.sHistTimes[2] + ',' + self.sHistTimes[3] + ',' + self.sHistTimes[4]
-                   + ',' + self.sHistStamps[0] + ',' + self.sHistStamps[1] + ',' + self.sHistStamps[2] + ',' + self.sHistStamps[3] + ',' + self.sHistStamps[4]).encode('utf-8'))
-##                c.send(sHistStamps[0] + ',' + sHistStamps[1] + ',' + sHistStamps[2] + ',' + sHistStamps[3] + ',' + sHistStamps[4])
+        if data == 'UP':
+            on_UP()
+        elif data == 'DOWN':
+            on_DOWN()
+        elif data == 'SET':
+            on_SET()
         else:
-            c.send(('I got:' + data).encode('utf-8'))
+            pass
         if not data:
             print(str(a[0]) + ':' + str(a[1]), "disconnected")
-            self.connections.remove(c)
+            connections.remove(c)
             c.close()
             break
+
+def broadcast(msg):
+    for c in connections:
+        c.send(msg.encode('utf-8'))
+
+def on_UP():
+    global iAddTime
+    if bSET:
+        iAddTime += 5
+        broadcast(str_allData())
+
+def on_DOWN():
+    global iAddTime
+    if bSET:
+        iAddTime -= 5
+        broadcast(str_allData())
+
+def on_SET():
+    global bSET
+    global iAddTime
+    global iCurrentTime
+    global fullHistData
+    if bSET:
+        bSET=False
+        if iAddTime != 0:
+            iCurrentTime += iAddTime
+            iAddTime = 0
+            timeStamp = adjustTime()
+            strToWrite = str(iCurrentTime)
+            for i in timeStamp:
+                strToWrite += ',' + str(i)
+            strToWrite += '\n'
+            text_insert(dataPath + '/' + fileName, strToWrite)
+            f = open(dataPath + '/' + fileName, 'r')
+            fullHistData = f.read()
+            f.close()
+    else:
+        bSET=True
+    broadcast(str_allData())
+    
+
+def text_insert(originalfilename,string):
+        with open(originalfilename,'r') as f:
+            with open('newfile.txt','w') as f2: 
+                f2.write(string)
+                f2.write(f.read())
+        os.rename('newfile.txt',originalfilename)
+
+#------------
+def checkData():
+    global fileName
+    global iCurrentTime
+    global fullHistData
+    
+    year = str(time.localtime()[0])
+    
+    if time.localtime()[1] < 10:
+        month = '0' + str(time.localtime()[1])
+    else:
+        month = str(time.localtime()[1])
+        
+    if time.localtime()[2] < 10:
+        day = '0' + str(time.localtime()[2])
+    else:
+        day = str(time.localtime()[2])
+                        
+    fileName = year + '_' + month + '_' + day + '.txt'
+    if not os.path.isdir(dataPath):
+        os.mkdir(dataPath)
+        print('Creating ./data folder...')
+        
+    try:
+        f = open(dataPath + '/' + fileName, 'r')
+        print('File for today was found.')
+    except FileNotFoundError:
+        print('File for today not found. Creating new file...')
+        f = open(dataPath + '/' + fileName, 'w')
+        f.write('15')
+        stamp = time.localtime()
+        for i in stamp:
+            f.write(',' + str(i))
+    fullHistData = f.read()
+    print(fullHistData)
+    iCurrentTime = int(fullHistData.split('\n')[0].split(',')[0])
+    print(iCurrentTime)
+    f.close()
+        
+def adjustTime():
+    tempHour = 0
+    tempMin = 0
+    
+    inTime = [0,0,0,0,0,0,0,0,0]
+    i = 0
+    while i < 9:
+        inTime[i] = time.localtime()[i]
+        i += 1
+    
+    if (inTime[5] + secOffset) > 59:
+        tempMin = 1
+        inTime[5] = inTime[5] + secOffset - 60
+    elif (inTime[5] + secOffset) < 0:
+        tempMin = -1
+        inTime[5] = inTime[5] + secOffset + 60
+    else:
+        inTime[5] = inTime[5] + secOffset
+    
+    if (inTime[4] + minOffset + tempMin) > 59:
+        tempHour = 1
+        inTime[4] = inTime[4] + minOffset + tempMin - 60
+    elif (inTime[4] + minOffset + tempMin) < 0:
+        tempHour = -1
+        inTime[4] = inTime[4] + minOffset + tempMin + 60
+    else:
+        inTime[4] = inTime[4] + minOffset + tempMin
+    
+    inTime[3] = inTime[3] + hourOffset + tempHour
+    return inTime
+
+def str_allData():
+    allData = str(bSET)+','+ str(iAddTime)+','+str(hourOffset)+','+str(minOffset)+','+str(secOffset)+'\n'
+    allData = allData +  fullHistData
+    return allData
+    
+    
+
+#--------------------------------------------------------
+#GLOABL VARIABLES
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connections = []
+iAddTime = 0
+iCurrentTime = 0
+bSET = False
+
+    #Clock offset CONSTANTS
+hourOffset = 0
+minOffset = 5
+secOffset = 30
+
+dataPath = './data'
+fileName = 'PLACEHOLDER'
+fullHistData = ''
+
+
+
+
+checkData()
+sock.bind(('0.0.0.0', 12343))
+sock.listen(1)
+startAccThread()
+try:
+    print('Running socket server...')
+    while True:
+        pass
+except KeyboardInterrupt:
+    print('\nUser Interrupt')
+    sock.close()
+    
